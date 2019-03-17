445cf7c6eb50cf581f6a15170aa9631bab88447d
Ygor Kayan <ygorkayan@gmail.com>
2019-03-17 16:52:00 -0300

    Update Telegram.py

--- Telegram.py
+++ Telegram.py
@@ -7,62 +7,57 @@ from Comandos import Comandos
 
 class Telegram:
 
+
     def __init__(self , ID):
         self.bot = telepot.Bot(ID)
 
+
     # Metodo para pega id, nome e Mensagem, vinda do Usuario 
-    def __MSG(self, msg): 
-        Mensagem = ""
+    def __msg_recebida(self, msg): 
         temp = msg["from"]
         id = temp["id"]
         nome = temp["first_name"]
-        #print(msg) ESSE COMANDO, IMPRIME O DICIONARIO VINDO DO TELEGRAM
         try:
             texto = msg["text"]
-            Mensagem = {"id":id, "nome":nome, "texto": texto} 
         except Exception:
             texto = "So é permitido Texto"
-
         Mensagem = {"id":id, "nome":nome, "texto": texto}
-        self.__DisparaMSG(Mensagem)
+        self.__tratar_msg(Mensagem)
+
 
     #Metodo para verificar comando, so funciona se Usuario for Admin
-    def __Verificar_Comando(self, usuario, texto):
-        Texto = str(texto)
-        Comando = Comandos()
+    def __verificar_comando(self, texto):
+            Comando = Comandos()
+            return Comando.Verificar(texto)
 
-        if Texto.startswith("/"):
-            if usuario:
-                return Comando.Verificar(Texto)
-            else:
-                return "Voce nao é Admin"
-        return Texto
 
     #Metodo para verificar se o usuario é admin
-    def __Verificar_Usuarios(self, id):
-        if str(id) == "ID DO USUARIO":
-            return True 
-        else:
-           return False
+    def __verificar_usuarios(self, id):
+       return True if id == 547939468 else False
 
-    # Metodo que trabalha com id, nome, texto, vindo do metodo __MSG
-    def __DisparaMSG(self, msg):
+
+    # Metodo que trabalha com id, nome, texto, vindo do metodo __mensagem_usuario
+    def __tratar_msg(self, msg):
         id = msg["id"]
         nome = msg["nome"]
         texto = msg["texto"]
-        print(id, nome, texto)
-        Super_Usuario = self.__Verificar_Usuarios(id)
-        texto = self.__Verificar_Comando(Super_Usuario, texto)
-        self.MandarMsg(id, texto)
+        #print(id, nome, texto)
+        super_usuario = self.__verificar_usuarios(id)
+        if texto.startswith("/") and super_usuario:
+            texto = self.__verificar_comando(texto)
+        else:
+            texto = "Comando nao existe, ou voce nao é Admin"
+        self.mandar_msg(id, texto)
    
+
    # Recebe um dicionario do Usuario
-    def RecebeMSG(self):
-        self.bot.message_loop(self.__MSG)
+    def receber_msg(self):
+        self.bot.message_loop(self.__msg_recebida)
         while True:  # Esse while esta sem otimizaçao, por esta em loop infinito, consome muito recuso
             pass     # Para melhora, pensei em fazer toda vez que ele entra no loop ele espera 1 segundo
                      # e ai proseguir, porem em Python nao sei fazer o Sleep(1000) do C#
-    #Manda Msg para usuario
-    def MandarMsg(self, id, msg):
-        self.bot.sendMessage(id, msg)
 
-   
+
+    #Manda Msg para usuario
+    def mandar_msg(self, id, msg):
+        self.bot.sendMessage(id, msg)
