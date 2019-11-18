31192e9ff5eb6a6e77de16a0f528b36bc2ff9f8a
Ygor Kayan <ygorkayan@gmail.com>
2019-11-18 18:42:03 -0300

    Delete Comandos.py

--- Comandos.py
+++
@@ -1,9 +0,0 @@
-class Comandos:
-
-    def Verificar(self, msg):
-        if msg == "/desligar_tela":
-            return "tela desligada"
-        elif msg == "/desligar_monitor":
-            return "Monitor desligado"
-        else:
-            return "Comando nao Conhecido"
